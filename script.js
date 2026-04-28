380b209261b7b5a397bbec0129a600aec718fb88
Jakob Vintera <38698352+Hokiloki@users.noreply.github.com>
2026-04-29 01:01:38 +0200

    Add pseudocode

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
 const historyPanel = document.querySelector(".historyPanel");
+
+ // >>> target displayNumber instead of display
+
+let firstNumber = 0;
+let secondNumber;
+
+//Initial display
+
+
+// zahl, zahl, zahl number concat bis user operator drückt, dann store in firstNumber (falls firstNumber leer ist) dann zahl, zahl, zahl number concat bis user operator oder = drückt, dann store in secondNumber(falls firstNumber voll ist) und ausrechnen, result bei display und historyPanel anzeigen, dann wird secondNumber zu firstNumber, falls dann eine zahl eingegeben wird wird sie in secondNumber gespeichert
+
+function numberInput(){
+  let concat = x;
+}
+
+/* buttons.addEventListener("click", () => {
+  if (event.target.id === "zero"){}
+  if (event.target.id === "comma")
+  if (event.target.id === "prefix")
+  if (event.target.id === "one")
+  if (event.target.id === "two")
+  if (event.target.id === "three")
+  if (event.target.id === "four")
+  if (event.target.id === "five")
+  if (event.target.id === "six")
+  if (event.target.id === "seven")
+  if (event.target.id === "eight")
+  if (event.target.id === "nine")
+}); */
+
+
