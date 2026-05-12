5ab1b53628c12a31855e376e99d47790e339fd16
Jakob Vintera <38698352+Hokiloki@users.noreply.github.com>
2026-05-12 17:00:19 +0200

    Add more buttons and input validation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const buttons = document.querySelectorAll("button");
+const numButtons = document.querySelectorAll(".numbers button");
+//const operatorButtons = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
 let displayNumber = document.querySelector("#displayNumber");
 const historyPanel = document.querySelector(".historyPanel");
@@ -10,19 +11,41 @@ let operator;
 // zahl, zahl, zahl number concat bis user operator drückt, dann store in firstNumber (falls firstNumber leer ist) dann zahl, zahl, zahl number concat bis user operator oder = drückt, dann store in secondNumber(falls firstNumber voll ist) und ausrechnen, result bei display und historyPanel anzeigen, dann wird secondNumber zu firstNumber, falls dann eine zahl eingegeben wird wird sie in secondNumber gespeichert
 
 
+
+
+function calculateResult(firstNumber, secondNumber, operator){
+
+}
+
+
+
+// Number Button Input Listener
+for (button of numButtons){
+  button.addEventListener("click", (event) => {
+    if (event.target.id === "prefix" && displayNumber.textContent != 0 && displayNumber.textContent[0] != "-"){
+      displayNumber.textContent = "-" + displayNumber.textContent;
+    } else if (event.target.id === "prefix" && displayNumber.textContent[0] === "-"){
+        displayNumber.textContent = displayNumber.textContent.slice(1);
+    } else if (event.target.id === "comma" && displayNumber.textContent.includes(".")){
+        return false;
+    } else if (event.target.id === "comma" && displayNumber.textContent === "0"){
+        displayNumber.textContent = "0."
+    } else {
+        let input = event.target.textContent;
+        concatNumber(input);}
+  })
+}
+
+
+//Number Assembler
 function concatNumber(number){
-  if (displayNumber.textContent == "0") {
+  if (displayNumber.textContent === "0") {
     displayNumber.textContent = number;
+  } else if (displayNumber.textContent.length === 15){
+      return false;
   } else {
     displayNumber.textContent += number;
-  }}
-
-// Button Input Listener
-for (button of buttons){
-  button.addEventListener("click", (event) => {
-      let input = event.target.textContent;
-      concatNumber(input);
-  })
+  }
 }
 
 /*
