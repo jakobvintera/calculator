c0014aea9bcc8b627e9211a933e01e5f90e562d9
Jakob Vintera <38698352+Hokiloki@users.noreply.github.com>
2026-05-12 15:52:00 +0200

    Add button interaction and display number

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display");
+let displayNumber = document.querySelector("#displayNumber");
 const historyPanel = document.querySelector(".historyPanel");
-
- // >>> target displayNumber instead of display
-
-let firstNumber = 0;
+let firstNumber;
 let secondNumber;
-
-//Initial display
+let operator;
 
 
 // zahl, zahl, zahl number concat bis user operator drückt, dann store in firstNumber (falls firstNumber leer ist) dann zahl, zahl, zahl number concat bis user operator oder = drückt, dann store in secondNumber(falls firstNumber voll ist) und ausrechnen, result bei display und historyPanel anzeigen, dann wird secondNumber zu firstNumber, falls dann eine zahl eingegeben wird wird sie in secondNumber gespeichert
 
-function numberInput(){
-  let concat = x;
+
+function concatNumber(number){
+  if (displayNumber.textContent == "0") {
+    displayNumber.textContent = number;
+  } else {
+    displayNumber.textContent += number;
+  }}
+
+// Button Input Listener
+for (button of buttons){
+  button.addEventListener("click", (event) => {
+      let input = event.target.textContent;
+      concatNumber(input);
+  })
 }
 
-/* buttons.addEventListener("click", () => {
-  if (event.target.id === "zero"){}
+/*
+
+  if (event.target.id === "zero"){
+    let input = 0;
+    console.log(input);
+    concatNumber(input);
+  }
+
   if (event.target.id === "comma")
   if (event.target.id === "prefix")
-  if (event.target.id === "one")
-  if (event.target.id === "two")
-  if (event.target.id === "three")
-  if (event.target.id === "four")
-  if (event.target.id === "five")
-  if (event.target.id === "six")
-  if (event.target.id === "seven")
-  if (event.target.id === "eight")
-  if (event.target.id === "nine")
-}); */
-
-
+ */
